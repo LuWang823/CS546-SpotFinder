c089b51305f8b94027f076755dc362da5f2d82dd
bpenman <bpenman@stevens.edu>
2024-12-03 14:20:32 -0500

    profile page + spot posting in progress

--- backend/src/routes/mainRoute.js
+++ backend/src/routes/mainRoute.js
@@ -23,8 +23,20 @@ mainRouter.route('/verify').get(async (req, res) => {
     title:"Verify Account"
   });
 });
-mainRouter.route('/test').post(async (req, res) => {
-  return res.status(200).json(req.body);
+mainRouter.route('/profile/:accessToken/:refreshToken').get(async (req, res) => {
+  //TODO: implement getUser(id) function to populate profile page
+    //dummy data for now...
+  return res.status(200).render('profile',{
+    title:"User Profile",
+    name: "John Doe",
+    email: "123@gmail.com",
+    verified: true,
+    hobbies: ['fishing', 'hiking', 'skateboarding'],
+    address: "123 Street Road, New York",
+    spots: [129883190,1290809120398,12498009812,912849012],//array of spot IDs that will be used to reach endpoint that gets spot based on ID
+    likedSpots: [1239881903,352145342,989298020], //array of spot id's again
+    visitedSpots: [12398231903,352145342,989798020] //array of spot id's again
+  });
 });
 
 
