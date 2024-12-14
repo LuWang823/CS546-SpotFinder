d4c8d3450a7f5d9f734814720eedd90a6e288282
bpenman <bp1023123@gmail.com>
2024-12-14 16:30:13 -0500

    like button add spot id to list of user's like spots, still needs to be represented on profile page

--- frontend/public/scripts/likeSpot.js
+++ frontend/public/scripts/likeSpot.js
@@ -1,11 +1,30 @@
-const like = async () =>{
-    //TODO: IMPLEMENT THIS FUNCTION
-    /*  essential info needed for function
-    const user = localStorage.getItem('accessToken')//use this access token in header
-    const refresh = localStorage.getItem('refreshToken')//use this refresh token in header
+const like = async () => {
     const currentUrl = window.location.pathname;
-    const id = currentUrl.split('/')[2]; // spot id from current url*/
+    const id = currentUrl.split('/')[2]; // spot id from current url
 
-    console.log('liked spot');
-    return
+    const errorMessage = document.getElementById('error-message');
+
+    try {
+        // Send spot to the backend
+        //http://localhost:3000/api/v1/users/like/:spotId
+        const response = await fetch((`http://localhost:3000/api/v1/users/like/${id}`), {
+            method: 'PATCH',
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+                Refresh: `Bearer ${localStorage.getItem('refreshToken')}`
+            },
+        });
+
+        if (response.ok) {
+
+        } else {
+            console.log(response);
+            errorMessage.textContent = 'Internal Server error';
+            errorMessage.style.display = 'block';
+        }
+    } catch (e) {
+        errorMessage.textContent = e.message;
+        errorMessage.style.display = 'block';
+    }
 }
