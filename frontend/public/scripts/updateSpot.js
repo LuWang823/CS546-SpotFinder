519e2b8fd0e599f82923a479e965572d368ba296
bpenman <bp1023123@gmail.com>
2024-12-15 17:53:44 -0500

    removed commented code and adjust wording of error message

--- frontend/public/scripts/updateSpot.js
+++ frontend/public/scripts/updateSpot.js
@@ -1,30 +1,5 @@
 const updateSpot = async (formData) => {
     try {
-    //TODO: IMPLEMENT THIS FUNCTION
-    /* most likely implementaion vv
-    const currentUrl = window.location.pathname;
-    const id = currentUrl.split('/')[2]; // spot id from current url
-    try {
-        // Send credentials to the backend
-        //http://localhost:3000/spots/:id
-        const response = await fetch((`http://localhost:3000/spots/${id}`), {
-            method: 'PATCH',
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-                Refresh: `Bearer ${localStorage.getItem('refreshToken')}`
-            },
-            body:JSON.stringify(spotObj)
-        });
-    
-        if (response.ok) {
-
-        } else {
-            throw new Error('Internal Server Error');
-        }
-    } catch (e) {
-        throw e
-    }*/
        // try to refresh access token
        const currentUrl = window.location.pathname;
        const id = currentUrl.split('/')[2]; // spot id from current url
