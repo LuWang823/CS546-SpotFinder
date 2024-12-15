4287548f3415f1683bd4bdcf3d76fe4fdc9becc5
Shirsha <shirsha.sen.1997@gmail.com>
2024-12-15 17:13:10 -0500

    Update spot

--- frontend/public/scripts/updateSpot.js
+++ frontend/public/scripts/updateSpot.js
@@ -1,4 +1,5 @@
-const updateSpot = async (spotObj, id) => {
+const updateSpot = async (formData) => {
+    try {
     //TODO: IMPLEMENT THIS FUNCTION
     /* most likely implementaion vv
     const currentUrl = window.location.pathname;
@@ -24,4 +25,25 @@ const updateSpot = async (spotObj, id) => {
     } catch (e) {
         throw e
     }*/
-}
+       // try to refresh access token
+       const currentUrl = window.location.pathname;
+       const id = currentUrl.split('/')[2]; // spot id from current url
+       console.log(formData);
+        axios.defaults.headers.common["Authorization"] =
+        `Bearer ${localStorage.getItem("accessToken")}`;
+      const { data } = await axios.patch(`http://localhost:3000/api/v1/spots/${id}`,
+        formData,
+        {
+          "Content-Type": "application/x-www-form-urlencode",
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          Refresh: `Bearer ${localStorage.getItem("refreshToken")}`,
+        },
+    );
+    return data;
+            
+} catch (e) {
+    console.error("Axios error: ", e.response ? e.response.data : e.message);;
+    throw e;
+  }
+};
+        
